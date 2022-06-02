77d5fe8f74f928c7b8cbe52d2aaf99dce80e3858
Hayden Ross <haydenross00@gmail.com>
2022-06-02 15:40:51 -0400

    Minor bug fix
    Now works with initial index !=0

--- ImageClassificationTool/main.py
+++ ImageClassificationTool/main.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-directory = "C:\\PATH_TO_IMAGES"
+directory = "C:\\Users\hayde\Documents\School\Summer2022\Comp472\Project\Dataset\Medical+mask\Medical mask\Medical Mask\images"
 startingIndex = 0 #CAN BE USED TO START AT A CERTAIN POSITION
 
 outputFolders = ["None","N95","Surgical","Cloth"] #MAKE THESE FOLDERS IN THE IMAGE PATH
@@ -9,9 +9,7 @@ counts = [0,0,0,0]
 
 list = os.listdir(directory)
 
-for i in range(list):
-    if i < startingIndex:
-        i = startingIndex
+for i in range(startingIndex, len(list)):
 
     filename = list[i]
 
